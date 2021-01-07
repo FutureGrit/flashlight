9ac9137821aa0f97212422b417970aef2aadf0e8
FutureGrit <app.futuregrit@gmail.com>
2021-01-07 18:15:10 +0530

    :bug: Fix sound not playing due to plugin missing error

--- android/app/src/main/kotlin/com/futuregrit/flashlight/MainActivity.kt
+++ android/app/src/main/kotlin/com/futuregrit/flashlight/MainActivity.kt
@@ -1,29 +1,22 @@
 package com.futuregrit.flashlight
 
-import android.os.Bundle
 import android.util.Log
-import io.flutter.app.FlutterActivity
-//import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "flashlight_activity"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(FlutterEngine(applicationContext))
-
-        MethodChannel(flutterView, CHANNEL).setMethodCallHandler { call, _ ->
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor, CHANNEL).setMethodCallHandler { call, _ ->
             if (call.method == "startFlashlight") {
                 startFlashlight()
             }
         }
-
     }
     private fun startFlashlight() {
         Log.e("ActivityCalled","*********** Hello From Android startFlashlight************");
